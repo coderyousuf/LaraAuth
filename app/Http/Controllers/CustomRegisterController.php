1b83af0fb68f763efaffbbe499310e43ca98d50d
coderyousuf <yousuf2mail@gmail.com>
2022-08-28 07:13:29 +0600

    lara auth

---
+++ app/Http/Controllers/CustomRegisterController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\LoginUserRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use App\Http\Requests\RegisterStoreRequest;
+
+class CustomRegisterController extends Controller
+{
+    public function registerFormShow(){
+        return view('custom-auth.register');
+    }
+    public function registerUser(RegisterStoreRequest $request){
+
+        // User create
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'phone' => $request->phone,
+            'password' => Hash::make($request->password),
+        ]);
+
+        // make a credentials array
+        $credentials = [
+            'email' => $request->email,
+            'password' => $request->password,
+        ];
+
+        // login attempt if success then redirect home
+        if(Auth::attempt($credentials)){
+            $request->session()->regenerate();
+            return redirect()->intended('home');
+        }
+
+        // return error message
+        return back()->withErrors([
+            'email' => 'Wrong Credentials found!'
+        ])->onlyInput('email');
+    }
+
+    public function loginFormShow(){
+        return view('custom-auth.login');
+    }
+    public function loginUser(LoginUserRequest $request){
+        // dd($request->all());
+        // make a credentials array
+        $credentials = [
+            'email' => $request->email,
+            'password' => $request->password,
+        ];
+
+        // login attempt if success then redirect home
+        if(Auth::attempt($credentials, $request->filled('remember'))){
+            $request->session()->regenerate();
+            return redirect()->intended('home');
+        }
+
+        // return error message
+        return back()->withErrors([
+            'email' => 'Wrong Credentials found!'
+        ])->onlyInput('email');
+    }
+
+    public function logout(Request $request){
+
+        Auth::logout();
+
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect()->route('login');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\CustomRegisterController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// Auth::routes();
+
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+// Auth::routes();
+
+
+
+Route::get('register', [CustomRegisterController::class, 'registerFormShow'])->name('register');
+Route::post('register', [CustomRegisterController::class, 'registerUser'])->name('register.store');
+Route::get('login', [CustomRegisterController::class, 'loginFormShow'])->name('login');
+Route::post('login', [CustomRegisterController::class, 'loginUser'])->name('login.store');
+
+Route::middleware(['auth'])->group(function(){
+    Route::post('logout', [CustomRegisterController::class, 'logout'])->name('logout');
+    Route::get('/home', [HomeController::class, 'index'])->name('home');
+});
+
+
+
